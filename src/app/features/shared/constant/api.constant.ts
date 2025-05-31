d7da6ef849303deb8fdd30db72ff7c044f7eb23a
jainath jha <85430936+jaijha1995@users.noreply.github.com>
2025-05-31 21:34:31 +0530

    Update api.constant.ts

--- src/app/features/shared/constant/api.constant.ts
+++ src/app/features/shared/constant/api.constant.ts
@@ -1,9 +1,9 @@
 export const API_CONSTANT = {
     login : 'superadmin/login/',
     addDevice : 'device/deviceadd/',
-    liveData : 'ws://3.110.185.148:8002/ws/',
-    command :'command/command/',
-    updateDeleteCommand : 'command/command/{id}/',
+    liveData : 'ws://3.110.185.148:8028/ws/',
+    command :'command/commands/',
+    updateDeleteCommand : 'command/commands/{id}/',
     commandDetails : 'connection/send-command/',
     cpuList : 'device/device/{id}/',
     updateCommandDetails :'connection/send-command/{id}/'
